d8b2bfca65a3b5468313b29f3840a2420cc55b8e
joao-biscaia <joaopedro.jpbf@gmail.com>
2026-05-14 11:36:30 -0300

    initial `routes.go` using `julienschmidt/httprouter` for routing and `justinas\alice` for middleware chaining

---
+++ cmd/api/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
+	router := httprouter.New()
+
+	standard := alice.New(app.logRequest)
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+
+	return standard.Then(router)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module greenlight.joaobiscaia.io
+
+go 1.26.2
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
+)
