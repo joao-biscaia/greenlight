cc4825eaace45ebfa75a5e0a4dad1164622692fc
joao-biscaia <joaopedro.jpbf@gmail.com>
2026-07-16 13:10:44 -0300

    add token authentication

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -53,3 +53,8 @@ func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Requ
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
+	message := "invalid authentication credentials"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
