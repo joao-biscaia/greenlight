3f1e7f10ef3e4d5f1a81a85155ecde2abad5753f
joao-biscaia <joaopedro.jpbf@gmail.com>
2026-07-19 23:58:09 -0300

    changing CORS middleware to treat preflight requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -167,6 +167,7 @@ func (app *application) requirePermission(code string, next http.Handler) http.H
 func (app *application) enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 
 		origin := r.Header.Get("Origin")
 
@@ -174,6 +175,14 @@ func (app *application) enableCors(next http.Handler) http.Handler {
 			for i := range app.config.cors.trustedOrigins {
 				if origin == app.config.cors.trustedOrigins[i] {
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						w.WriteHeader(http.StatusOK)
+						return
+					}
 					break
 				}
 			}
